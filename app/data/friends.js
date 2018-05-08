89aeb0a44406d6b1b6d73df67b8aad9586a9e926
lwerner27 <lwerner27@gmail.com>
2018-05-08 13:51:21 -0500

    Updated the html pages

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+const friendsArr = [
+    {
+        name: "Andrew",
+        photo: "https://bit.ly/2wq2ukM",
+        scores: [
+            1,
+            4,
+            5,
+            3,
+            2,
+            5,
+            4,
+            3,
+            2,
+            1
+        ]
+    }
+]
+
+module.exports = friendsArr
